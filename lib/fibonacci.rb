0a1c6ccef24dd1c4a37e86a6eb64f0980b740b47
gracexinran <gracexinran@gmail.com>
2019-11-14 11:33:42 -0800

    Fibonacci completed

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -3,6 +3,12 @@
 # Time Complexity - ?
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
-def fibonacci(n)
- 
+def fibonacci(n, current = 2, solution = [0, 1])
+  raise ArgumentError if n < 0
+  return n if n == 0 || n == 1
+  if n == current
+    return solution[n - 1] + solution[n - 2]
+  end
+  solution << solution[current - 1] + solution[current - 2]
+  fibonacci(n, current + 1, solution)
 end
