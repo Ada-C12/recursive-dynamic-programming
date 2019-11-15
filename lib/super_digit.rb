b92d9051b369266677a907bd80e58c7a5d198b50
Paige Davenport <davenport.paige@outlook.com>
2019-11-15 13:48:50 -0800

    Added method for super digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,9 +1,18 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n) - where n is the number of digits
+# Space Complexity - O(n) - where n is the number of digits
 def super_digit(n)
-   
+  if (super_helper(n) / 10) == 0
+    return super_helper(n)
+  else
+    return super_digit(super_helper(n))
+  end
+end
+
+def super_helper(n)
+  return n if n/10 == 0
+  return (n % 10) + super_helper(n/10)
 end
   
 
