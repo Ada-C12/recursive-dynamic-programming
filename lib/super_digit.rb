05a5bb3fdb30c57654eb9a668c05a1b511da3db4
Dianna Tingg <dtingg@gmail.com>
2019-11-15 15:02:43 -0800

    finished super digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,21 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  if n < 10
+    return n
+  else
+    super_digit(n % 10 + super_digit(n/10))
+  end
 end
-  
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def refined_super_digit(n, k)
-    
+  number_string = n * k
+  
+  number = number_string.to_i
+  
+  super_digit(number)
 end
-    
