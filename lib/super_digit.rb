46e6742a40370b65c95522d4c678cdd1d2fe6cf6
KKennedyCodes <holmstedtk@gmail.com>
2019-11-15 15:22:59 -0800

    Add Methods Added

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,13 +3,27 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
-end
+  if n.class != Array
+    n = n.to_s.split('')
+  end
   
+  sum = 0
+  n.each do |value|
+    sum += value.to_i
+  end
+  
+  if sum < 10
+    return sum
+  else
+    return super_digit(sum)
+  end
+end
+
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+  number = n * k
+  final = super_digit(number)
+  return final
 end
-    
