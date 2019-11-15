0442f4d334a4cefcd27df6cc25af30ae51f6dbfd
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-15 11:14:37 -0800

    implemented super_digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,13 +3,24 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  while (n / 10) != 0
+    n = super_digit_helper(n)
+  end
+  return n
 end
-  
+
+def super_digit_helper(n) 
+  if (n / 10) == 0
+    return n
+  else
+    last_digit = (n % 10)
+    n = (n / 10)
+    return last_digit + super_digit_helper(n)
+  end
+end
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+  
 end
-    
