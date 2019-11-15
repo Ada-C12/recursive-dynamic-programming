7ba515db55fd184aaacde15fdd45506cb0a4df27
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-11-15 13:37:25 -0800

    Fibonacci method passes tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,26 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
-# Hint, you may want a recursive helper method
+# Time Complexity - O(n) where n is the n passed as an argument. fib_helper is called n times.
+# Space Complexity - 0(n), the solutions array will have a length of 2 regardless of how large or small n is.
+
 def fibonacci(n)
- 
+  return fib_helper([0,1], 2, n)
 end
+
+def fib_helper(solutions, current, n)
+  raise ArgumentError if n < 0
+
+  if n == 0 || n == 1
+    return n
+  end
+
+  if current == n
+    return solutions[0] + solutions[1]
+  end
+
+  solutions << solutions[0] + solutions[1]
+  solutions.delete_at(0)
+
+  return fib_helper(solutions, current + 1, n)
+end
+
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,13 +3,24 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  sum = 0
+
+  while n > 0
+    sum += n % 10
+    n /= 10
+  end
+
+  if sum < 10 
+    return sum
+  else
+    return super_digit(sum)
+  end
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(log n + log m) where n is the n passed in and m is the super_digit solution of n.
+# Space Complexity - O(log n) because the 
 def refined_super_digit(n, k)
-    
+  return super_digit(super_digit(n) * k)
 end
     
