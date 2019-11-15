0b3c52413931e231ee244e459ff85f7505a185f0
Paige Davenport <davenport.paige@outlook.com>
2019-11-15 12:11:28 -0800

    Added method for fibonnaci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -4,5 +4,23 @@
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
 def fibonacci(n)
- 
+  if n < 0
+    raise ArgumentError
+  else
+    return fib_helper([0,1], 2, n)
+  end
+end
+
+def fib_helper(solutions, current, n)
+  return n if n == 0 || n == 1
+
+  if current == n
+    return solutions[0] + solutions[1]
+  end
+  
+  new_solution = solutions[0] + solutions[1]
+  solutions[0] = solutions[1]
+  solutions[1] = new_solution
+
+  return fib_helper(solutions, current + 1, n) if n > current
 end
