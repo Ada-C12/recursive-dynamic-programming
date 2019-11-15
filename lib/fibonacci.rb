d9610338ef9e3440bfe41c7d02f1768b01ae8877
Janice Huang <janicehuang@gmail.com>
2019-11-14 17:25:38 -0800

    start fib, get tests passing on super_digit

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,15 @@
-# Improved Fibonacci
+# # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
-# Hint, you may want a recursive helper method
-def fibonacci(n)
- 
-end
+# # Time Complexity - ?
+# # Space Complexity - ?  (should be O(n))
+# # Hint, you may want a recursive helper method
+# def fibonacci(n)
+#     return 0 if n == 0
+#     return 1 if n == 1
+#     # return fib(n-1) + fib(n-2)
+#   end
+# end
+
+# def fib_helper(n)
+#   fib_saver = []
+# end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,13 +3,21 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  return n if n < 10
+  super_digit(digitize(n))
 end
-  
+
+def digitize(n)
+  return n if n < 10
+  sum = 0
+  single_digit = n % 10
+  sum += single_digit
+  return sum + digitize(n / 10)
+end
+
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+
 end
-    
