612fb474785a113d0681e917cfcf734695a817c9
Monick <monick.keo@gmail.com>
2019-11-15 11:25:41 -0800

    added time and space complexity

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,23 +1,8 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n) since it will access the hash or run the recursion based on the number.
+# Space Complexity - O(n) since the stack will take up memory and the hash takes up memory but it takes it linearly. (should be O(n))
 # Hint, you may want a recursive helper method
-# def fibonacci(n)
-#   raise ArgumentError.new if n < 0;
-#   return n if n == 0 || n == 1;
-#   return fib_helper([0, 1], 2, n);
-# end
-
-# def fib_helper(solutions, current, n)
-#   if current == n
-#     return solutions[n - 1] + solutions[n-2]
-#   end
-
-#   solutions << solutions[current - 1] + solutions[current -2]
-#   return fib_helper(solutions, current + 1, n)
-# end
-
 def fibonacci(n, fib_hash={})
   raise ArgumentError.new if n < 0
   return n if n <= 1
