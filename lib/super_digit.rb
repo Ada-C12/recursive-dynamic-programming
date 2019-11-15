0da2838966ec531da855e2fba7a5043f75f44c46
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-15 14:51:21 -0800

    added refined_super_digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -19,8 +19,23 @@ def super_digit_helper(n)
   end
 end
 
+# def super_digit(n)
+#   if n < 10
+#     return n
+#   else
+#     super_digit(n % 10 + super_digit(n / 10))
+#   end
+# end
+
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
+  concatenated_n = ''
+  while k > 0
+    concatenated_n += n.to_s
+    k -= 1
+  end
   
+  concatenated_n = concatenated_n.to_i
+  return super_digit(concatenated_n)
 end
