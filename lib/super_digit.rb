57e75e0a5c588e394468f3dd9f988896a07a78ce
Monick <monick.keo@gmail.com>
2019-11-15 11:38:25 -0800

    did superdigit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,9 +1,19 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n^2)
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  n = n.to_s.split('')
+  sum = 0 
+  n.each do |num|
+    num = num.to_i
+    sum += num
+  end 
+
+  if sum.to_s.length > 1 
+    return super_digit(sum)
+  end 
+  return sum
 end
   
 
