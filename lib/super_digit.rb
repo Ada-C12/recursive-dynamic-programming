fc23597f7bf32ef3bf483acf7eaf19a14d4ffef9
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-11-15 22:26:45 -0800

    added refined super digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,26 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(log10(n))
+# Space Complexity - O(log10(n))
 def super_digit(n)
-   
-end
+  if n < 10
+    return n
+  end
   
-
-# Time Complexity - ?
-# Space Complexity - ?
-def refined_super_digit(n, k)
-    
+  # return super_digit(sum_of_digits(n))
+  return super_digit((n % 10) + super_digit(n / 10))
 end
-    
+
+# def sum_of_digits(n)
+#   if n < 10
+#     return n
+#   end
+
+#   return (n % 10) + sum_of_digits(n / 10)
+# end
+
+# Time Complexity - O(log10(n))
+# Space Complexity - O(log10(n))
+def refined_super_digit(n, k)
+  return super_digit(super_digit(n) * k)
+end
