e0d20f0269e6066a34f513c044845b8da1b0e369
Nicky Choi <nickyjinchoi@gmail.com>
2019-11-16 14:02:54 -0800

    created super digit and refined super digit  methods

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,17 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(log n) each time we're dividing by 10 after each recursive call
+# Space Complexity - O(log n)
 def super_digit(n)
-   
-end
-  
+  return n if n % 10 == n
 
-# Time Complexity - ?
-# Space Complexity - ?
-def refined_super_digit(n, k)
-    
+  return super_digit(n % 10 + super_digit(n / 10))
 end
-    
+
+# Time Complexity - O(log n) each time we're dividing by 10 after each recursive call
+# Space Complexity - O(log n)
+def refined_super_digit(n, k)
+  return n if n % 10 == n || k % 10 == n
+
+  return super_digit(k * n % 10 + super_digit(k * n / 10))
+end
