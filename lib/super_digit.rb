0570bd094614b38800787034a7d98aa3fa23d657
Alice <alicesunhi@gmail.com>
2019-11-17 14:57:16 -0800

    complete

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,32 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(log n)
+# Space Complexity - O(log n)
 def super_digit(n)
-   
-end
+  return n if n / 10 < 1
   
-
-# Time Complexity - ?
-# Space Complexity - ?
-def refined_super_digit(n, k)
-    
+  string = n.to_s.split("")
+  sum = 0
+  
+  string.each do |num|
+    sum += num.to_i
+  end
+  
+  return super_digit(sum)
 end
-    
+
+
+# Time Complexity - O(log n)
+# Space Complexity - O(log n)
+def refined_super_digit(n, k)
+  num = super_digit(n)
+  
+  return super_digit(k*num)
+end
+
+
+# n = 148
+# k = 4
+
+# superdigit(148148148148)
+# = superdigt( superdigit(148) +  superdigit(148) + superdigit(148) + superdigit(148))
