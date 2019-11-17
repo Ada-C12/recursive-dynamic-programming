5facdf1c364613d7b6c671b7cb6170bcebaa04a0
Natalie Tapias <nataliemtapias@gmail.com>
2019-11-16 16:48:20 -0800

    added the time and space complexities

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,12 +1,13 @@
 # Improved Fibonacci
 
 # Time Complexity - O(n)
-# Space Complexity - ?  O(n)
+# Space Complexity - ? O(1)
 # Hint, you may want a recursive helper method
 def fibonacci(n)
   if n < 0 
     raise ArgumentError
   end
+
   return fib_helper(num_minus_1 = 1, num_minus_2 = 0, 2, n)
 end
 
