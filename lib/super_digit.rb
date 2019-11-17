5e0fc43ea0b783c4595a672f9b49cb84198f81ea
Sam <samcoll@Samanthas-MBP.localdomain>
2019-11-17 12:26:19 -0800

    created methods super_digit and refined_super_digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,30 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
-def super_digit(n)
-   
+# Time Complexity - O(n)
+# Space Complexity - O(1)
+def super_digit(n)    
+  until n <= 9
+    n = n.digits.sum
+  end
+  return n
 end
-  
 
-# Time Complexity - ?
-# Space Complexity - ?
-def refined_super_digit(n, k)
-    
+
+# Time Complexity - O(n+m) where m = k
+# Space Complexity - O(n)
+def refined_super_digit(n, k)  
+  num = ""
+  k.times do
+    num += "#{n}" 
+  end 
+  
+  n = num.to_i
+  until n <= 9
+    n = n.digits.sum
+  end
+  return n
 end
-    
+
+# pp super_digit(9875)
+# pp refined_super_digit(9875, 4)
+# pp refined_super_digit(148, 3)
