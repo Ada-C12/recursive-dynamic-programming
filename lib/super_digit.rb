ec1338a0e7a50e18391a456a0ed8333c2cc1c9af
geli-gel <angele.zam@gmail.com>
2019-11-17 15:59:28 -0800

    tests passing

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,18 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n^2)
+# Space Complexity - O(n^2)
 def super_digit(n)
-   
+  n_array = n.to_s.split('').map {|i| i.to_i}
+  return n if n_array.length == 1
+  return super_digit(n_array.sum)
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n^2)
+# Space Complexity - O(n^2)
 def refined_super_digit(n, k)
-    
+  new_n = (n.to_s * k).to_i
+  return super_digit(new_n)    
 end
     
