0ca11d93d9ab0cf6e30e5a58328b9a5ab0293320
Janice Huang <janicehuang@gmail.com>
2019-11-16 17:34:14 -0800

    fib tests all passing

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -19,5 +19,13 @@ end
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
+  super_digit(n * k)
+end
 
+def digitize(n)
+  return n if n < 10
+  sum = 0
+  single_digit = n % 10
+  sum += single_digit
+  return sum + digitize(n / 10)
 end
