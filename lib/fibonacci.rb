fd8ec8d3db43a6fbbea0e274a31b171738aaf061
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-17 10:07:47 -0800

    Completed improved fibonacci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,41 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n). This method will run n-2 times.
+# Space Complexity -  O(n). The reference value of the fib_addends array is being changed with every call stack, but no additional arrays are being created. (should be O(n))
 # Hint, you may want a recursive helper method
-def fibonacci(n)
- 
+
+# fib_addends is defined with the numbers to make Fib(2)
+def fibonacci(n, fib_addends = [0, 1])
+  raise ArgumentError if n < 0
+  return 0 if n == 0
+  return 1 if n == 1
+  
+  fib = fib_addends[0] + fib_addends[1]
+  # returns fib once n has decremented to 2, because the method begins calculating at Fib(2) not Fib(0)
+  return fib if n == 2
+  
+  fib_addends[0] = fib_addends[1]
+  fib_addends[1] = fib
+  return fibonacci(n-1, fib_addends)
 end
+fibonacci(5)
+
+
+## WITH HELPER METHOD
+
+# def fibonacci(n)
+#   return 0 if n == 0
+#   return 1 if n == 1
+
+#   fib_addends = [0, 1]
+
+#   def fib_helper(fib_addends, n)
+#     fib = fib_addends[0] + fib_addends[1]
+#     return fib if n == 2
+
+#     fib_addends[0] = fib_addends[1]
+#     fib_addends[1] = fib
+#     return fib_helper(fib_addends, n-1)
+#   end
+#   fib_helper(fib_addends, n)
+# end
