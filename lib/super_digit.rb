09e31ae529cfc923ce0b519a3e43ea921d419cdc
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-17 10:23:30 -0800

    Basic super digit method added

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,9 +3,21 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  if n > 9
+    p n % 10
+    n = n % 10 + super_digit(n / 10)
+    if n > 9
+      super_digit(n)
+    else 
+      p n
+      return n
+    end
+  else
+    p n
+    return n
+  end
 end
-  
+# super_digit(90210)  
 
 # Time Complexity - ?
 # Space Complexity - ?
