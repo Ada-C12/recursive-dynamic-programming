e914f33e61c3a3ae493ac3d14deea73c8b523fa8
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-17 11:09:12 -0800

    Added working super digit methods with convoluted time complexity explanations

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,27 +1,22 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - I think the worst case time complexity is n log n, with n being the number of digits in the n integer. The method recursively calls the super_digit method n times to begin with, and the result of the super_digit method will trigger the while loop again if it is greater than 9 (which it will only do for 2 digits, not 1, hence log n). So, worst case O(n log n).
+# Space Complexity - O(n)
 def super_digit(n)
-  if n > 9
-    p n % 10
+  while n > 9
     n = n % 10 + super_digit(n / 10)
-    if n > 9
-      super_digit(n)
-    else 
-      p n
-      return n
-    end
-  else
-    p n
-    return n
   end
+  return n
 end
-# super_digit(90210)  
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - Because I used the super_digit method as a helper method, it inherits its time complexity of O(n log n), the new time complexity is k(n log n), where k is the number of digits in the k integer.
+
+# Space Complexity - O(n). Nothing crazy happening with space.
 def refined_super_digit(n, k)
-    
+  result = super_digit(n) * k
+  while result > 9
+    result = super_digit(result)
+  end
+  return result
 end
-    
+
