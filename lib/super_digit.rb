11f2abe1d80fd0f50a4cd6bea61610d33cdaacf4
Michaela Morrow <morrow.michaela@gmail.com>
2019-11-16 19:37:09 -0800

    Solved problems

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,21 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n * m) where m is related to the number of digits in the sum of the digits
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  return n if n / 10 == 0
+  return super_digit(digit_helper(n))
 end
-  
 
-# Time Complexity - ?
-# Space Complexity - ?
-def refined_super_digit(n, k)
-    
+def digit_helper(n, sum = 0)
+  return (sum + n) if n / 10 == 0
+  sum += n % 10
+  n = n / 10
+  return digit_helper(n, sum)
 end
-    
+
+# Time Complexity - O(n * m)
+# Space Complexity - O(n)
+def refined_super_digit(n, k)
+  return super_digit(k * digit_helper(n))
+end
