8dd80729b577dbb92ff82793391a768bf56d5f77
Diana <dianah46@gmail.com>
2019-11-17 19:34:50 -0800

    passed superdigit tests, will try to return later for refined superdigit method

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,24 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  return n if n.to_s.length == 1 
+
+  return super_digit(digit_helper(n))
 end
   
+def digit_helper(n)
+  digits = n.to_s.split("")
+  result = digits.map {|element| element.to_i}
+  return result.sum
+end
+
+
 
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
-    
-end
+# def refined_super_digit(n, k)
+  
+# end
     
