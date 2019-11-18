6a4eecdc75dc6f0f9adff75cb8c10177589ad271
daniela sanchez <dnsanche@gmail.com>
2019-11-18 09:16:05 -0800

    Minor change

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,12 +1,12 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
+# Time Complexity - O(n)
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
 def fibonacci(n)
   return 0 if n == 0
   return 1 if n == 1
   raise ArgumentError if n < 0
-
+  
   return fibonacci(n-2) + fibonacci(n-1)
 end
