c67143a0950327945beb54b6d38b90aff3623bf2
brilatimer <brihiramine@gmail.com>
2019-11-18 07:42:39 -0800

    fib and super_digit complete

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,25 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 # Hint, you may want a recursive helper method
 def fibonacci(n)
- 
+  if n < 0
+    raise ArgumentError
+  end
+  return fib_helper(n, {})
+end
+
+def fib_helper(n, hash)
+  if hash.key? n
+    return hash[n]
+  end
+  
+  # basecase
+  if n == 0 || n == 1
+    return n
+  end
+  
+  hash[n] = fib_helper(n - 1, hash) + fib_helper(n - 2, hash)
+  return hash[n]
 end
