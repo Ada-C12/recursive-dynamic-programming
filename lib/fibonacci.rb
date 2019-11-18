e68add479fff06f03ab7f3fb12042550ebdba891
Janice Huang <janicehuang@gmail.com>
2019-11-17 18:44:24 -0800

    tried to answer time and space complexity questions

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,6 +1,6 @@
 # # Improved Fibonacci
 
-# # Time Complexity - ?
+# # Time Complexity - O(n)
 # # Space Complexity - ?  (should be O(n))
 # # Hint, you may want a recursive helper method
 
@@ -12,10 +12,8 @@ end
 def fib_helper(fib_saver, current, n)
   return n if n == 0 || n == 1
   return fib_saver.sum if current == n
-
   new_sum = fib_saver.sum
   fib_saver[0] = fib_saver[1]
   fib_saver[1] = new_sum
-  # is this better/different than `fib_saver = [fib_saver[1], new_sum]`?
   return fib_helper(fib_saver, current + 1, n)
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,7 +1,7 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(nlogn)?
+# Space Complexity - O(logn)?
 def super_digit(n)
   return n if n < 10
   super_digit(digitize(n))
@@ -16,16 +16,8 @@ def digitize(n)
 end
 
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - I'm pretty sure this solution doesn't reduce the time complexity, unfortunately. So it's O(knlogn)?
+# Space Complexity - O(logn)?
 def refined_super_digit(n, k)
   super_digit(n * k)
-end
-
-def digitize(n)
-  return n if n < 10
-  sum = 0
-  single_digit = n % 10
-  sum += single_digit
-  return sum + digitize(n / 10)
 end
