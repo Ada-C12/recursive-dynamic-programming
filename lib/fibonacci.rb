850cfa405d3168bb7720f33c529adfd19e6b6ea9
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-11-17 19:15:24 -0800

    super digit with recursion

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,29 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)
+# Space Complexity - O(n)  (should be O(n))
 # Hint, you may want a recursive helper method
+
+def fibonacci(target, ultimate = 1, pentultimate = 0, n = 2)
+  if target < 0
+    raise ArgumentError
+  elsif target == 0
+    return 0
+  elsif target == 1
+    return 1
+  end
+  
+  if n == target
+    current = pentultimate + ultimate
+    return current
+  else
+    n += 1
+    holder = pentultimate + ultimate
+    pentultimate = ultimate
+    ultimate = holder
+    return fibonacci(target, ultimate = ultimate, pentultimate = pentultimate, n = n)
+  end
+end
 
 # # # # brute force
 # def fibonacci(target)
@@ -30,24 +51,3 @@
 #   return current
 
 # end
-
-def fibonacci(target, ultimate = 1, pentultimate = 0, n = 2)
-  if target < 0
-    raise ArgumentError
-  elsif target == 0
-    return 0
-  elsif target == 1
-    return 1
-  end
-  
-  if n == target
-    current = pentultimate + ultimate
-    return current
-  else
-    n += 1
-    holder = pentultimate + ultimate
-    pentultimate = ultimate
-    ultimate = holder
-    return fibonacci(target, ultimate = ultimate, pentultimate = pentultimate, n = n)
-  end
-end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,30 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
-def super_digit(n)
-   
+# Time Complexity - O(n)
+# Space Complexity - O(log n)
+
+def super_digit(input)
+  # # # # brute recursion
+  if input < 10
+    return input
+  elsif input >= 10
+    sum = 0
+    i = 0
+    limit = input.to_s.length
+    dig_input = input.to_s
+    until i == limit
+      sum += dig_input[i].to_i
+      i += 1
+    end
+    return super_digit(sum)
+  end
 end
-  
+
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+  
 end
-    
+
+
