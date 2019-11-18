e2fc08ab329f540c662bc483f1342d3ac9e3355f
ohcloud <cloudylopez@icloud.com>
2019-11-18 15:09:08 -0800

    completed

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,18 @@
 # Improved Fibonacci
 
 # Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Space Complexity - O(n)
 # Hint, you may want a recursive helper method
 def fibonacci(n)
- 
+  if n in results
+    return results(n)
+  else
+    if n == 0
+      return 0
+      if n == 1 
+        return 1
+      end
+    end
+  end
+  results[n] = fib(n-1) + fib(n-2)
 end
