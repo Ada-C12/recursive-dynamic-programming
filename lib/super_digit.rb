b04daeff9d12c02dcfa3ec17ee585dbd60afca66
daniela sanchez <dnsanche@gmail.com>
2019-11-17 19:47:54 -0800

    Redifined Super Digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,13 +3,14 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-  
+
   return n if [0,1,2,3,4,5,6,7,8,9].include?(n) == true
 
-  num = n.digits
   sum = 0
-  num.each do |digit|
-    sum += digit
+  until n == 0 do
+    reminder = n % 10
+    sum += reminder
+    n = n/10
   end
 
   super_digit(sum) 
@@ -19,6 +20,23 @@ end
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+
+  return n  if [0,1,2,3,4,5,6,7,8,9].include?(n) == true && k == 1
+  
+  sum = 0
+  until n == 0 do
+    reminder = n % 10
+    sum += reminder
+    n = n/10
+  end
+
+  super_digit = sum * k
+  refined_super_digit(super_digit, 1)
 end
-    
+
+
+
+
+
+
+
