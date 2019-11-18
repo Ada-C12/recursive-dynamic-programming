483f0b877f539cc379e1d15631506d27e19a6024
Morgan Schuler <morganlschuler@gmail.com>
2019-11-17 16:49:58 -0800

    created the recursive methods

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,14 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(log(n) - reduced by a factor of 10 
+# Space Complexity - o(n)? Since we are not creating anything new
 def super_digit(n)
-   
+  n < 10 ? n : super_digit(n / 10 + n % 10)
 end
-  
+
 
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
-    
-end
-    
+# def refined_super_digit(n, k)
+
+# end
