704ee96ccd1704b5b353c1c2e440a50f3cbbbf3a
Diana <dianah46@gmail.com>
2019-11-17 19:40:45 -0800

    refactored fibonacci code

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -4,19 +4,22 @@
 # Space Complexity - O(n)
 # Hint, you may want a recursive helper method
 def fibonacci(n)
-  return fib_helper(n)
-end
-
-def fib_helper(n, solutions = [0,1], current = 2)
   if n < 0
     raise ArgumentError
   elsif n == 0 || n == 1
     return n
-  elsif n == current
+  end
+
+  return fib_helper(n)
+end
+
+def fib_helper(n, solutions = [0,1], current = 2)
+  if n == current
     return solutions[n - 1] + solutions[n - 2]
   end
 
   solutions << solutions[current - 1] + solutions[current - 2]
+  
   return fib_helper(n, solutions, current + 1)
 end
 
