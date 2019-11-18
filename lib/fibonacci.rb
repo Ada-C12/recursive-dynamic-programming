49c0619196db81365dc394270ef673df71df673a
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-17 16:52:54 -0800

    Updated test code to for 'return 5 for fib(5) to reflect test name and passing all fibonacci tests.

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,25 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
-# Hint, you may want a recursive helper method
+# Time Complexity - O(n) because it performs n operations
+# Space Complexity - O(n) because it makes n calls on the stack
+
 def fibonacci(n)
- 
+  return fib_helper(n, n_minus_2 = 0, n_minus_1 = 1, 2)
 end
+
+def fib_helper(n, n_minus_2, n_minus_1, current) # starting values (4, 0, 1, 2)
+  
+  # base cases
+  
+  raise ArgumentError if n < 0
+  return n if n == 0 || n == 1
+  return n_minus_1 + n_minus_2 if current == n
+
+  # recursive step -- increment current and reassign n_minus_2 and n_minus_1
+  current += 1 
+  fibber = n_minus_1 
+  n_minus_1 = n_minus_1 + n_minus_2 
+  n_minus_2 = fibber 
+  return fib_helper(n, n_minus_2, n_minus_1, current)   
+end
+
