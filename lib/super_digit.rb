bf62b0a5e7ce83a955232acff43513c3f13c62e7
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-18 16:59:07 -0800

    Passing tests for super digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,21 @@
 # Superdigit
 
-# Time Complexity - ?
+# Time Complexity - O log n 
 # Space Complexity - ?
-def super_digit(n)
-   
+def super_digit(n) 
+  # base case
+  if n <= 9
+    return n
+  else
+    # recursive step -- convert number to array of integers and add them all together
+    n_array = n.digits
+    n = n_array.sum
+    super_digit(n)
+  end
 end
-  
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+  
 end
-    
