3dde629dd446c9d142278f9d84e82fa4c3ae571f
Yasmin <yasmin312002@hotmail.com>
2019-11-19 00:46:32 -0800

    all tests passed

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,33 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - o(n)
+# Space Complexity - o(1)
 def super_digit(n)
-   
+  new_num = 0
+  numbers = n.to_s.split("")
+  if numbers.length <= 1
+    return n
+  end
+  numbers.each do |i|
+    new_num += i.to_i
+  end
+  return super_digit(new_num)
 end
-  
+   
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+  new_num = 0
+  times = k
+  numbers = n.to_s.split("")
+  if numbers.length <= 1
+    return n
+  end
+  numbers.each do |i|
+    new_num += i.to_i
+  end
+    new_n = times * new_num
+    return super_digit(new_n) 
 end
     
