3b6501faae4a59d3baf2ba35dae490b93e52a8c1
Eve Le <nguyenthao.lee@gmail.com>
2019-11-18 21:38:47 -0800

    Implemented super_digit method

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,13 +3,19 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+    return n if n/10 == 0
+
+    sum = n % 10
+    while n/10 != 0
+        n = n/10
+        sum += n % 10
+    end
+    return super_digit(sum)
 end
-  
+
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+
 end
-    
