06e9d09d5aba4636ac655a21a864ec4e30d997f6
eaball35 <eaball35@gmail.com>
2019-11-19 12:30:31 -0800

    completed super_digits

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,29 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  return n if n < 10
+  
+  sum = 0
+  n.to_s.each_char do |num|
+    sum += num.to_i
+  end
+    
+  return super_digit(sum)
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
-def refined_super_digit(n, k)
-    
+# Time Complexity - O(n)
+# Space Complexity - O(n)
+def refined_super_digit(n, k, i = 0)
+  return n if n < 10
+  
+  sum = 0
+  n.to_s.each_char do |num|
+    sum += num.to_i
+  end
+  
+  sum *= k if i == 0
+  return refined_super_digit(sum, k, i += 1)
 end
-    
