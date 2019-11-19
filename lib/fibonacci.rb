68f7f7b343ae879e306433d6da09d523344a7005
daniela sanchez <dnsanche@gmail.com>
2019-11-18 20:55:36 -0800

    Fibonnaci with recursive solution and dynamic programming

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -4,9 +4,17 @@
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
 def fibonacci(n)
-  return 0 if n == 0
-  return 1 if n == 1
+  return fib_helper( [0,1], 2, n )
+end
+  
+def fib_helper( solutions, current, n )
+  return n if n == 0 || n == 1
   raise ArgumentError if n < 0
   
-  return fibonacci(n-2) + fibonacci(n-1)
+  if current == n
+    return solutions[n - 1] + solutions[n - 2]
+  end
+
+  solutions << solutions[current - 1] + solutions[current -2]
+  return fib_helper(solutions, current + 1, n)
 end
