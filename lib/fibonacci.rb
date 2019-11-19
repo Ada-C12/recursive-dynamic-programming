a60d4da0bc470fa7f2d24743bb0c6e85912b4535
Dominique <dom_taylor@icloud.com>
2019-11-18 19:02:19 -0800

    finished

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,14 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity O(n)  (should be O(n))
+# Time Complexity - O(n!)
+# Space Complexity O(n) 
 # Hint, you may want a recursive helper method
 def fibonacci(n)
- 
+  if n < 0
+    raise ArgumentError, "Invalid number"
+  elsif n == 0 || n == 1
+    return n
+  else
+    return fibonacci(n - 2) + fibonacci(n - 1)
+  end 
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,10 +1,30 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
-def super_digit(n)
-   
+# Time Complexity - O(n)
+# Space Complexity - O(n)
+def super_digit(x)
+   if x < 10
+    return x
+  elsif
+    sum = integer_calcuation(x)
+    super_digit(sum)
+  end 
 end
+
+def integer_calcuation(x)
+  
+  digit_array = x.to_s.split('')
+
+  sum = 0
+
+  digit_array.each do |element|
+    sum += element.to_i
+  end 
+
+  return sum
+end 
+
+
   
 
 # Time Complexity - ?
