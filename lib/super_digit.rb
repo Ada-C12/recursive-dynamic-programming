f1c108652a5b45684e72180603d219f765a492dc
Farah Davoodi <farahdavoodi@gmail.com>
2019-11-18 21:19:02 -0800

    fixed superdigit method

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -5,12 +5,12 @@
 def super_digit(n)
   # first modulo (n % 10) and add to a counter(number)
   # until n%10 is equal 0
-  # then 
-
-  number = n.to_s.split("")
+  
   total = 0
-  number.each do |num|
-    total += num.to_i
+  until n % 10 == 0
+    counter = n % 10
+    total += counter
+    n = n/10
   end
 
   if total > 9
@@ -21,19 +21,15 @@ def super_digit(n)
 end
   
 
-# Time Complexity - O(k+n) where k is equal k value passed in the method and n is equal to the number of digits in the n-number passed in super_digit method
+# Time Complexity - O(n) where n is equal to the number of digits in the n-number passed in the super_digit method or the number of digits in the result-number passed in the super_digit method.
 # Space Complexity - O(n) where n is equal to the number of times the suger_digit method is placed on the call stack
 def refined_super_digit(n, k)
   if k == 1
     return super_digit(n)
+  else
+    result = super_digit(n)
+    result *= k
+    return super_digit(result)
   end
-
-  num = n.to_s
-  while k != 1
-    n = n.to_s + num
-    k -= 1
-  end
-
-  return super_digit(n.to_i)
 end
     
