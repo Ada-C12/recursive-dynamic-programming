dffd3ef776088772d27e4a2a8a5a9a0f32377da1
v <vanessareigosa@gmail.com>
2019-11-18 16:42:42 -0800

    finished fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -4,5 +4,20 @@
 # Space Complexity - ?  (should be O(n))
 # Hint, you may want a recursive helper method
 def fibonacci(n)
- 
+  return fib_helper(0, 1, 2, n)
 end
+
+def fib_helper(more_left, left, current, n)
+  if n < 0 
+    raise ArgumentError
+  end 
+  
+  return n if n == 0 || n == 1
+
+  if current == n 
+    return more_left + left
+  end 
+
+  
+  return fib_helper(left, more_left + left , current + 1, n)
+end 
