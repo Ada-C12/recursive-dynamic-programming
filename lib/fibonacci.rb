dd57fffb397c090571e0a2eb87e36b6f3ea90501
Mariya Burrows <mariya.burrows@gmail.com>
2019-11-18 20:48:54 -0800

    completed dynamic programming hw

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,8 +1,30 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - ?  O(n)
+# Space Complexity - ?  O(n)
 # Hint, you may want a recursive helper method
 def fibonacci(n)
- 
+  return fib_helper({}, n)
+  
+end
+
+
+def fib_helper(solutions, n)
+  if solutions.include?(n)
+    return solutions[n]
+  end
+  
+  if n == 0
+    solutions[n] = 0
+    return 0 
+  elsif n == 1
+    solutions[n] = 1
+    return 1 
+  elsif n >= 2
+    solutions[n] = (fib_helper(solutions, n-2) + fib_helper(solutions, n-1))
+    return solutions[n]
+  else
+    raise ArgumentError
+  end
+  
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,9 +3,30 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
+  super_digit_helper({}, n)
    
 end
-  
+
+def super_digit_helper(solutions, n)
+  if solutions[n]
+    return solutions[n]
+  end
+
+  if n.to_s.length == 1
+    solutions[n] = n
+    return solutions[n]  
+  end
+
+  string_num = n.to_s 
+
+  sum = 0
+  string_num.length.times do |i|
+    sum += (string_num[i].to_i)
+  end
+
+  solutions[n] = super_digit_helper(solutions, sum)
+  return solutions[n]
+end
 
 # Time Complexity - ?
 # Space Complexity - ?
