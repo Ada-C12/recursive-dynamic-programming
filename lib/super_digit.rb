29cc616d7f7c1581704022f877d00b2c1e6fbc43
Eve Le <nguyenthao.lee@gmail.com>
2019-11-18 22:16:11 -0800

    Implemented refined_super_digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,7 +1,7 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(logn) n is the number itself
+# Space Complexity - O(logn)
 def super_digit(n)
     return n if n/10 == 0
 
@@ -14,8 +14,13 @@ def super_digit(n)
 end
 
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(logn) n is the number itself
+# Space Complexity - O(logn)
 def refined_super_digit(n, k)
-
+    return n if k == 1 && n/10 == 0
+    sum = 0
+    k.times do
+        sum += super_digit(n)
+    end
+    return super_digit(sum)
 end
