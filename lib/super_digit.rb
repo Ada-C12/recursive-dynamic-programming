37ca94fa1533a84ca4463fa6ad9d00e29d2a7b95
Erika <erika.maust@gmail.com>
2019-11-20 10:54:40 -0800

    One digit superdigit passing

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,13 +3,15 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+   if n < 10
+    return n
+   end 
 end
   
 
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
+# def refined_super_digit(n, k)
     
-end
+# end
     
