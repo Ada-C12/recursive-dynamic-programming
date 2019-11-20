aad732ad7acfd4b240378af4e613b127adccb82d
Erika <erika.maust@gmail.com>
2019-11-20 11:10:51 -0800

    Added time and space complexity

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,7 +1,7 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n) ... It's dependent on the length of the string, but also only by half...so I think still O(n)?
+# Space Complexity - I thin O(1) because you're not creating any structures to calculate it
 def super_digit(n)
    if n < 10
     return n
