9588427944ff24a59f5b93caafb4c40a0ea8c6a9
Rinostar <codingrinostar@gmail.com>
2019-11-29 19:36:15 -0800

    2. refactor 'super_digit' - big O

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,15 @@
 # Superdigit
 
-# Time Complexity - O(n^2): n is the numer of digits for input integer
-# Space Complexity - O(n): call stack for recursion, number of digits
+# Time Complexity - O(log10(n)): n is the numer of digits for input integer
+# Space Complexity - O(log10(n)): call stack for recursion, number of digits
 def super_digit(n)
   return n if n.digits.length == 1
   n = n.digits.sum
   return super_digit(n)
 end
 
-# Time Complexity - O((log10 n)^2 + (log10 k)^2): n is input n, k is the input k
-# Space Complexity - O(n): call stack for recursion
+# Time Complexity - O(log10(n) + log10(k)): n is input n, k is the input k
+# Space Complexity - O(log10(n) + log10(k)): call stack for recursion
 def refined_super_digit(n, k)
   n = super_digit(n) * k
   return super_digit(n)
