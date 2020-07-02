6108372a5a498e22b948d7152075052b7be6230c
Erika Maust <jamjar@Erikas-MacBook-Pro.local>
2020-07-02 00:24:21 -0700

    fibonacci updated

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,27 +1,15 @@
-require 'pry'
+
 # Improved Fibonacci
 
-# Time Complexity - I think O(n)?
-# Space Complexity - TThere's an array being created, but it isn't dependent on the length of the array, so I think O(1)
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 
 # Hint, you may want a recursive helper method
 def fibonacci(n)
-  return fib_helper(n, fib_array = [0,1], fib = 2)
+
+  return 1 if n == 1
+  return 0 if n == 0
+
+  fibonacci(n - 2) + fibonacci(n - 1)
+  
 end
-
-def fib_helper(n, fib_array, fib)
-  if n < 0
-    raise ArgumentError
-  elsif n == 0 || n == 1
-    return n
-  elsif fib == n 
-    return fib_array[1] + fib_array[0]
-  end
-
-  temp = fib_array[1]
-  fib_array[1] = fib_array[1] + fib_array[0]
-  fib_array[0] = temp
-
-  return fib_helper(n, fib_array, fib + 1)
-end
-
