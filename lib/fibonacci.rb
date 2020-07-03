2f8ca429752bd64577dc68cada9f5c04d59c4c7b
Erika Maust <jamjar@Erikas-MacBook-Pro.local>
2020-07-03 02:11:53 -0700

    Fixed fibonacci, completed super_digit and refined_super_digit

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -7,9 +7,11 @@
 # Hint, you may want a recursive helper method
 def fibonacci(n)
 
-  return 1 if n == 1
+  return 1 if n == 1 || n == 2
   return 0 if n == 0
+  raise ArgumentError if n < 0 
 
-  fibonacci(n - 2) + fibonacci(n - 1)
+  fibonacci(n - 1) + fibonacci(n - 2)
   
 end
+
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,20 +1,23 @@
 # Superdigit
 
-# Time Complexity - O(n) ... It's dependent on the length of the string, but also only by half...so I think still O(n)?
-# Space Complexity - I thin O(1) because you're not creating any structures to calculate it
-def super_digit(n)
-   if n < 10
-    return n
-   end 
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 
-   
-   return super_digit(n.digits.sum)
+def super_digit(n)
+
+   return n if n < 10
+   super_digit(n.digits.sum)
+
 end
-  
 
 # Time Complexity - ?
 # Space Complexity - ?
-# def refined_super_digit(n, k)
-    
-# end
-    
+def refined_super_digit(n, k)
+
+   n = n.digits.sum * k
+   super_digit(n)
+
+end
+
+
+p refined_super_digit(3212, 4) 
